8aab37c5666931952d4b95f0c2baab2ead3efe85
LoraxLoft <samsungi9500164@gmail.com>
2020-10-12 23:58:10 +0300

    shit works
    P.S. this is kinda strong shit

--- main.cpp
+++ main.cpp
@@ -6,17 +6,18 @@
 using namespace std;
 
 void urovnenie(vector<int>& a, vector<int>& b){
-    if (a.size() == b.size()) {
+    int a_s = a.size(), b_s = b.size();
+    if (a_s == b_s) {
         return;
     } else {
         vector<int> buffer;
         buffer.push_back(0);
-        if (a.size() > b.size()){
-            for(int i = 0; i<a.size() - b.size() + 1; i++){
+        if (a_s > b_s){
+            for(int i = 0; i<(a_s - b_s); i++){
                 b.insert(b.begin(), buffer.begin(), buffer.end());
             }
         } else {
-            for(int i = 0; i<b.size() - a.size() + 1; i++){
+            for(int i = 0; i<b_s - a_s; i++){
                 a.insert(a.begin(), buffer.begin(), buffer.end());
             }
         }
@@ -41,76 +42,96 @@ void normalise(vector<int>& vec){
 vector<int> summ(vector<int> a, vector<int> b){
     urovnenie(a,b);
     vector<int> res;
-for(int i = 0; i < a.size(); i++){
+    for(int i = 0; i < a.size(); i++){
         res.push_back(a[i]+b[i]);
     }
     normalise(res);
     return res;
 }
 
-void sdvig(vector<int>& a, int n){
-    int pustie_rozryadi = n/4;
-    a[a.size()-1] *= pow(10,n%4);
-    normalise(a);
-    for(int i = 0; i<pustie_rozryadi; i++){
-        a.push_back(0);
+vector<int> subtr(vector<int> a, vector<int> b){
+    urovnenie(a, b);
+    vector<int> res, buff(1);
+    for(int i = a.size()-1; i>=0; i--){
+        if (a[i] < b[i]){
+            a[i-1]-=1;
+            a[i] += 10000;
+        }
+        buff[0] = a[i] - b[i];
+        res.insert(res.begin(), buff.begin(), buff.end());
+        buff[0] = 0;
     }
+    return res;
 }
 
+vector<int> sdvig(vector<int> a, int n){
+    for(int i = 0; i<n; i++){
+        a.push_back(0);
+    }
+    return a;
+}
 
-vector<int> multipl(string a, string b){
-    if (a.size() <= 4){
+vector<int> multipl(vector<int> a, vector<int> b){
+    urovnenie(a, b);
+    if (a.size() == 0){
         vector<int> res;
-        int result = stoi(a) * stoi(b);
+        return res;
+    }
+    if (a.size() == 1){
+        vector<int> res;
+        int result = a[0] * b[0];
         res.push_back(result);
         normalise(res);
         return res;
     } else {
-        string a1, a0, b1, b0;
-        vector<int> a1b1, a0b0, a1_a0b1_b0;
-        a1 = a.substr(0, a.size()/2);
-        a0 = a.substr(a.size()/2,a.size());
-        b1 = a.substr(0, b.size()/2);
-        b0 = a.substr(b.size()/2,b.size());
-        
-        return a1b1;
+        vector<int> a1b1, a0b0, abmini, a1, b1, a0, b0;
+        a1.insert(a1.begin(), a.begin(), a.begin()+a.size()/2);
+        a0.insert(a0.begin(), a.begin()+a.size()/2, a.end());
+        b1.insert(b1.begin(), b.begin(), b.begin()+b.size()/2);
+        b0.insert(b0.begin(), b.begin()+b.size()/2, b.end());
+
+        a1b1 = multipl(a1, b1);
+        a0b0 = multipl(a0, b0);
+        abmini = multipl(summ(a1, a0), summ(b1, b0));
+/*
+        cout<<"a1b1:  ";
+        for(int i: a1b1){cout<<i<<' ';}
+        cout<<"\na0b0:  ";
+        for(int i: a0b0){cout<<i<<' ';}
+        cout<<"\nabmini:  ";
+        for(int i: abmini){cout<<i<<' ';}
+*/
+        return summ(summ(sdvig(a1b1, (a.size() - a.size()/2)*2), a0b0),sdvig(subtr(subtr(abmini, a1b1), a0b0), a.size() - a.size()/2)) ;
     }
 }
 
-class Karatsuba{
-
-    vector <int> n,m;
-    string c,d;
-public:
-    Karatsuba(const string& ch1,const string& ch2){
-        c = ch1; d = ch2;
-        for(int i = 0; i< ch1.size(); i+=1){
-            n.push_back(stoi(ch1.substr(i, 1)));
-        }
-        for(int i = 0; i< ch2.size(); i+=1){
-            m.push_back(stoi(ch1.substr(i, 1)));
-        }
+vector<int> separator(const string& s){
+    vector<int> res;
+    res.push_back(stoi(s.substr(0, s.size()%4)));
+    for(int i = 0; i<s.size()/4; i++){
+        res.push_back(stoi(s.substr(s.size()%4 + i*4, 4)));
     }
-
-    void show(){
-        vector<int> n;
-        for(int i : n){
-            cout << i<< ' ';
-        }
-    }
-
-};
+    return res;
+}
 
 int main(){
     string x, y;
-    /*
-    x = "12345678900000000";
-    y = "321";
+    x = "9";
+    y = "9876543219876543213345663424526357764648646123465612345672345999999999999999999999999999999999999";
 
+    vector<int> chuva, ch ,lol;
+    chuva = separator(x);
+    ch = separator(y);
+    lol = multipl(chuva, ch);
+    for(int i:lol){
+        cout<< i<< ' ';
+    }
+    cout<<'\n'<<lol.size();
+/*
     Karatsuba karatel = Karatsuba(x,y);
 
     karatel.show();
-    */
+
     vector<int> sum;
     vector<int> chuva(3);
     chuva[0] = 1234;
@@ -133,5 +154,6 @@ int main(){
     for(int i: sum){
         cout<< i<< ' ';
     }
+    */
     return 0;
 }
