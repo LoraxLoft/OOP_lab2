eab71bb4354be2ca6ea224e691369e2034e057b3
LoraxLoft <samsungi9500164@gmail.com>
2020-10-18 17:35:05 +0300

    gona start Toom
    some problems solved
    
    new main0() to check if all works
    it does not give spare 0 at the start

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <ctime>
+#define BASE 10
+#define base_len 1
 
 using namespace std;
+
+// have to deal with 0-s
+
+
+class VeryLong{
+
+    vector<int> value;
+
+public:
+
+    VeryLong(){
+        value.push_back(0);
+    }
+
+    VeryLong(string num){
+        value.push_back(stoi(num.substr(0, num.size()%base_len)));
+        for(int i = 0; i<num.size()/base_len; i++){
+            value.push_back(stoi(num.substr(num.size()%base_len + i*base_len, base_len)));
+        }
+    }
+
+
+
+};
 
 void urovnenie(vector<int>& a, vector<int>& b){
     int a_s = a.size(), b_s = b.size();
@@ -28,21 +54,24 @@ void normalise(vector<int>& vec){
     int nosilki = 0;
     for(int i = vec.size()-1; i>=0; i--){
         vec[i]+=nosilki;
-        nosilki = vec[i]/10000;
-        vec[i] %= 10000;
+        nosilki = vec[i]/BASE;
+        vec[i] %= BASE;
     }
-    while (nosilki){
+    while (nosilki!=0){
         vector<int> nos;
-        nos.push_back(nosilki%10000);
-        nosilki/=10000;
+        nos.push_back(nosilki%BASE);
+        nosilki/=BASE;
         vec.insert(vec.begin(), nos.begin(), nos.end());
+    }
+    while(vec.size()!=1 && vec[0] == 0){
+        vec.erase(vec.begin());
     }
 }
 
 vector<int> summ(vector<int> a, vector<int> b){
     urovnenie(a,b);
     vector<int> res;
-    for(int i = 0; i < a.size(); i++){
+for(int i = 0; i < a.size(); i++){
         res.push_back(a[i]+b[i]);
     }
     normalise(res);
@@ -54,29 +83,33 @@ vector<int> subtr(vector<int> a, vector<int> b){
     vector<int> res, buff(1);
     for(int i = a.size()-1; i>=0; i--){
         if (a[i] < b[i]){
+            if (i==0){
+                cout<<"Subst problems";
+            }
             a[i-1]-=1;
-            a[i] += 10000;
+            a[i] += BASE;
         }
         buff[0] = a[i] - b[i];
         res.insert(res.begin(), buff.begin(), buff.end());
         buff[0] = 0;
     }
+    normalise(res);
     return res;
 }
 
 vector<int> sdvig(vector<int> a, int n){
+    if(n==0){return a;}
     for(int i = 0; i<n; i++){
         a.push_back(0);
     }
     return a;
 }
 
-vector<int> multipl(vector<int> a, vector<int> b){
+
+
+vector<int> KaratsubaMultipl(vector<int> a, vector<int> b, int depth = 0){
+    //cout<< "depth now:"<< depth<<endl;
     urovnenie(a, b);
-    if (a.size() == 0){
-        vector<int> res;
-        return res;
-    }
     if (a.size() == 1){
         vector<int> res;
         int result = a[0] * b[0];
@@ -84,76 +117,84 @@ vector<int> multipl(vector<int> a, vector<int> b){
         normalise(res);
         return res;
     } else {
-        vector<int> a1b1, a0b0, abmini, a1, b1, a0, b0;
+        vector<int> a1b1, a0b0, abmini, a1, b1, a0, b0, res;
         a1.insert(a1.begin(), a.begin(), a.begin()+a.size()/2);
         a0.insert(a0.begin(), a.begin()+a.size()/2, a.end());
         b1.insert(b1.begin(), b.begin(), b.begin()+b.size()/2);
         b0.insert(b0.begin(), b.begin()+b.size()/2, b.end());
 
-        a1b1 = multipl(a1, b1);
-        a0b0 = multipl(a0, b0);
-        abmini = multipl(summ(a1, a0), summ(b1, b0));
-/*
-        cout<<"a1b1:  ";
-        for(int i: a1b1){cout<<i<<' ';}
-        cout<<"\na0b0:  ";
-        for(int i: a0b0){cout<<i<<' ';}
-        cout<<"\nabmini:  ";
-        for(int i: abmini){cout<<i<<' ';}
-*/
-        return summ(summ(sdvig(a1b1, (a.size() - a.size()/2)*2), a0b0),sdvig(subtr(subtr(abmini, a1b1), a0b0), a.size() - a.size()/2)) ;
+        a1b1 = KaratsubaMultipl(a1, b1, depth+1);
+        a0b0 = KaratsubaMultipl(a0, b0, depth+1);
+        abmini = KaratsubaMultipl(summ(a1, a0), summ(b1, b0), depth+1);
+
+        res = summ(summ(sdvig(a1b1, (a.size() - a.size()/2)*2), a0b0),sdvig(subtr(subtr(abmini, a1b1), a0b0), a.size() - a.size()/2));
+
+        return res;
+    }
+}
+
+vector<int> SchoolMultipl(vector<int> a, vector<int> b){
+    vector<int> res;
+    res.push_back(0);
+    int a_s=a.size(), b_s=b.size();
+    if (a_s>=b_s){
+        for(int i=b_s-1; i>=0; i--){
+            vector<int> buffbig, buffsmal;
+            for(int j=a_s-1; j>=0; j--){
+                buffsmal.push_back(a[j]*b[i]);
+                buffbig.insert(buffbig.begin(), buffsmal.begin(), buffsmal.end());
+                buffsmal.erase(buffsmal.begin());
+            }
+            normalise(buffbig);
+            res = summ(sdvig(buffbig,b_s-i-1), res);
+        }
+        return res;
+    }else{
+        return SchoolMultipl(b,a);
     }
 }
 
 vector<int> separator(const string& s){
     vector<int> res;
-    res.push_back(stoi(s.substr(0, s.size()%4)));
-    for(int i = 0; i<s.size()/4; i++){
-        res.push_back(stoi(s.substr(s.size()%4 + i*4, 4)));
+    if (s.size()%base_len != 0){
+        res.push_back(stoi(s.substr(0, s.size()%base_len)));
+    }
+    for(int i = 0; i<s.size()/base_len; i++){
+        res.push_back(stoi(s.substr(s.size()%base_len + i*base_len, base_len)));
     }
     return res;
 }
 
 int main(){
     string x, y;
-    x = "9";
-    y = "9876543219876543213345663424526357764648646123465612345672345999999999999999999999999999999999999";
+    x = "11";
+    y = "19";
 
     vector<int> chuva, ch ,lol;
     chuva = separator(x);
     ch = separator(y);
-    lol = multipl(chuva, ch);
+    unsigned int str = clock();
+    lol = KaratsubaMultipl(chuva, ch);
+    unsigned int end = clock();
     for(int i:lol){
         cout<< i<< ' ';
     }
-    cout<<'\n'<<lol.size();
-/*
-    Karatsuba karatel = Karatsuba(x,y);
+    cout<<'\n'<<end-str<<'\n';
+    return 0;
+}
 
-    karatel.show();
 
-    vector<int> sum;
-    vector<int> chuva(3);
-    chuva[0] = 1234;
-    chuva[1] = 1568;
-    chuva[2] = 9876;
-    vector<int> chuvak(4);
-    chuvak[0] = 1234;
-    chuvak[1] = 1568;
-    chuvak[2] = 9876;
-    chuvak[3] = 1234;
-    sum = summ(chuva, chuvak);
-    for(int i: chuva){
-        cout<< i<< ' ';
+int main0(){
+    unsigned long long z;
+    z = pow(2, 10);
+    for(unsigned long long i=100; i<=z;i++){
+        for(unsigned long long j = 100;j<=z;j++){
+            if (KaratsubaMultipl(separator(to_string(i)), separator(to_string(j))) == SchoolMultipl(separator(to_string(i)), separator(to_string(j)))){continue;}
+            else{
+                cout<<i<< ','<< j<< "Houston, we've got a problem";
+                break;
+            }
+        }
     }
-    cout<< endl;
-    for(int i: chuvak){
-        cout<< i<< ' ';
-    }
-    cout<< endl;
-    for(int i: sum){
-        cout<< i<< ' ';
-    }
-    */
     return 0;
 }
